8b4cb13613684ae0a5fefb7f5730f46c9397e941
Apurva Memani <amemani2018@pingry.org>
2016-03-06 18:40:01 -0500

    Concentrated lines, got rid of unecessary text

--- Coin.py
+++ Coin.py
@@ -1,29 +1,24 @@
 #Author: Apurva Memani
-#Date: February 2, 2016
+#Date: March 6, 2016
 import random
 prediction = raw_input("What do you think the coin will be? Please use heads or tails. ")
 flip = random.randint(0,1)
 #this makes the randomization of the coin flip
-term = ("heads") 
-
-term2 = ("tails")
-
-#sets what terms we are looking for in prediction
 if (flip == 0):
 	print("The coin is Heads")
 	
 elif (flip == 1):
 		print("The coin is Tails")
 #tells what each side is
-if term == prediction and (flip == 0):
+if 'heads' == prediction and (flip == 0):
 	print("You are correct!")
 	
-elif term2 == prediction and (flip == 0):
+elif 'tails' == prediction and (flip == 0):
 	print("You are incorrect")
 #checks if your prediction is right or wrong
-if term == prediction and (flip == 1):
+if 'heads' == prediction and (flip == 1):
 	print("You are incorrect!")
 
-elif term2 == prediction and (flip == 1):
+elif 'tails' == prediction and (flip == 1):
 	print("You are correct!")
 #checks if your prediction is right or wrong 
